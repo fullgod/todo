ccf2d2027ebc47df59a44f5ed4f78db95ae030f3
fullgod <fullgod@gmail.com>
2015-05-26 22:19:37 +0400

    copy

--- public_html/mytinytodo.js
+++ public_html/mytinytodo.js
@@ -1,8 +1,3 @@
-/*
-	This file is part of myTinyTodo.
-	(C) Copyright 2009-2010 Max Pozdeev <maxpozdeev@gmail.com>
-	Licensed under the GNU GPL v2 license. See file COPYRIGHT for details.
-*/
 
 (function(){
 
